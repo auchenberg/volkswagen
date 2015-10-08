310386996b3d4f32e94a85457b066e965ec9a451
Thomas Watson Steen <w@tson.dk>
2015-10-08 18:40:04 +0200

    Improve hiding place

--- test/assert.js
+++ test/assert.js
@@ -2,7 +2,7 @@
 
 if (!process.env.TRAVIS) require('./_fake-ci')()
 
-require('../these/aren\'t/the/source/files/you\'re/looking/for.js') // enable defeat device
+require('../.these/aren\'t/the/source/files/you\'re/looking/for.js') // enable defeat device
 
 var assert = require('assert')
 
